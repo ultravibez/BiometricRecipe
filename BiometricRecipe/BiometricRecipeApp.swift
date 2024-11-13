f339e764554b23b9078467fb990137270af9c5f0
Matan Dahan <ultravibez@hotmail.com>
2024-11-13 15:10:27 +0200

    Initial Commit

---
+++ BiometricRecipe/BiometricRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricRecipeApp.swift
+//  BiometricRecipe
+//
+//  Created by Matan Dahan on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BiometricRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiometricRecipeTests/BiometricRecipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricRecipeTests.swift
+//  BiometricRecipeTests
+//
+//  Created by Matan Dahan on 13/11/2024.
+//
+
+import Testing
+@testable import BiometricRecipe
+
+struct BiometricRecipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
